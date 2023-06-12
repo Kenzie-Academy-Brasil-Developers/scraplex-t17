2696f812ef72be8884dc6aab33ded6c39226269c
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-06-12 10:43:56 -0300

    fix identation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { RoutesMain } from "./routes/RoutesMain"
-import { Link } from "react-router-dom"
-import { GlobalStyle } from "./styles/global"
-import { ResetStyle } from "./styles/reset"
-import { Header } from "./components/Header"
+import { RoutesMain } from "./routes/RoutesMain";
+import { GlobalStyle } from "./styles/global";
+import { ResetStyle } from "./styles/reset";
+import { Header } from "./components/Header";
 
-function App() {  return (
-    <div className="App">
-      <GlobalStyle />
-      <ResetStyle />
-      <Header />
-      <RoutesMain />
-    </div>
-  )
+function App() {
+   return (
+      <div className="App">
+         <GlobalStyle />
+         <ResetStyle />
+         <Header />
+         <RoutesMain />
+      </div>
+   );
 }
 
-export default App
+export default App;
